0223fba4aa10f663136483e97c5532665f3fc0b0
pengumaxi <pengumaxi@pop-os.localdomain>
2022-11-02 20:22:44 +0100

    commit

--- csvtojson.py
+++ csvtojson.py
@@ -4,15 +4,15 @@ import json
 
 
 def csvToJson(csv_path, json_path):
-    jsonData = {}
+    jsonData = []
 
 
     with open(csv_path, encoding="utf-8") as csvfile:
 
         csvData = csv.DictReader(csvfile)
+
         for rows in csvData:
-            key = rows['Filename']
-            jsonData[key] = rows
+            jsonData.append(rows)
 
     with open(json_path,'w', encoding='utf-8') as jsonfile:
         jsonfile.write(json.dumps(jsonData, indent = 5))
