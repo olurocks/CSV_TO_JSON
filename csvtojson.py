42a68f179ec10b4596b68a7db88e4b67eb578fff
pengumaxi <pengumaxi@pop-os.localdomain>
2022-11-02 18:42:19 +0100

    initial commit

---
+++ csvtojson.py
@@ -0,0 +1,24 @@
+import csv
+import json
+
+
+
+def csvToJson(csv_path, json_path):
+    jsonData = {}
+
+
+    with open(csv_path, encoding="utf-8") as csvfile:
+
+        csvData = csv.DictReader(csvfile)
+        for rows in csvData:
+            key = rows['']
+            jsonData[key] = rows
+
+    with open(json_path,'w', encoding='utf-8') as jsonfile:
+        jsonfile.write(json.dumps(jsonData, indent = 5))
+
+csv_path = r'teamNames.csv'
+json_path = r'jsonfile.json'
+
+csvToJson(csv_path, json_path)
+
